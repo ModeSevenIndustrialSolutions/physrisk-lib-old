ee0ec8d958dbbee10c6f53436b1901a4596e9dbd
Matthew Watkins <mwatkins@linuxfoundation.org>
2023-11-27 18:30:37 +0000

    Chore: Fix shellcheck errors in test rename script
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

--- scripts/test-rename.sh
+++ scripts/test-rename.sh
@@ -4,18 +4,17 @@
 
 if [ $# -ne 1 ]; then
     echo "Usage:	$0 [test folder]"; exit 1
-elif [ ! -d $1 ]; then
+elif [ ! -d "$1" ]; then
     echo "Error: specified target was not a folder"; exit 1
 else
     # Target specified was a folder
-    TARGET=$1
+    TARGET="$1"
 fi
 
 # Forward
-for TEST in $(find $TARGET -name 'test*.py' | xargs)
-do
-    PREFIX_STRIP="${TEST/test_/}"
-    NEW_TEST="${PREFIX_STRIP/.py/_test.py}"
+for TEST in $(find "$TARGET" -print0 -name "test_*.py" | xargs); do
+    PREFIX_STRIP=$(echo "$TEST" | sed "s/^test_//}")
+    NEW_TEST=$(echo "$PREFIX_STRIP" | sed "s/.py/_test.py/}")
     echo "Renaming $TEST -> $NEW_TEST"
     git mv "${TEST}" "${NEW_TEST}"
 done
