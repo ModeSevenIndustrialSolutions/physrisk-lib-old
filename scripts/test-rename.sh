d7efde5da8aede25a8c95cca26eb169d5f0a9bc2
Matthew Watkins <mwatkins@linuxfoundation.org>
2023-11-27 17:58:04 +0000

    Chore: Update test renaming script with space, not tabs
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

--- scripts/test-rename.sh
+++ scripts/test-rename.sh
@@ -3,19 +3,19 @@
 # set -x
 
 if [ $# -ne 1 ]; then
-	echo "Usage:	$0 [test folder]"; exit 1
+    echo "Usage:	$0 [test folder]"; exit 1
 elif [ ! -d $1 ]; then
-	echo "Error: specified target was not a folder"; exit 1
+    echo "Error: specified target was not a folder"; exit 1
 else
-	# Target specified was a folder
-	TARGET=$1
+    # Target specified was a folder
+    TARGET=$1
 fi
 
 # Forward
 for TEST in $(find $TARGET -name 'test*.py' | xargs)
 do
- 	PREFIX_STRIP="${TEST/test_/}"
-	NEW_TEST="${PREFIX_STRIP/.py/_test.py}"
-	echo "Renaming $TEST -> $NEW_TEST"
-	git mv "${TEST}" "${NEW_TEST}"
+    PREFIX_STRIP="${TEST/test_/}"
+    NEW_TEST="${PREFIX_STRIP/.py/_test.py}"
+    echo "Renaming $TEST -> $NEW_TEST"
+    git mv "${TEST}" "${NEW_TEST}"
 done
